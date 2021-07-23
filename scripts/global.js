2d720fa98e05650f1a64a006bff21e4ead8221c5
Robert Spier <robert.spier@gmail.com>
2021-07-23 16:06:23 +0200

    restruct

---
+++ scripts/global.js
@@ -0,0 +1,50 @@
+const APP = {
+	NAME                  : 'artaphine',
+	VERSION               : '1.0.1',
+	AUTHOR                : 'Robert Spier',
+	CREATION_DATE         : new Date().getFullYear(),
+};
+
+class App {
+	constructor() {
+		console.log(`${APP.NAME} ${APP.VERSION}, © ${APP.CREATION_DATE} ${APP.AUTHOR}`);
+		this.glitch();
+	}
+
+	glitch() {
+		var imagePath = 'images/bg.jpg';
+		var imgContainerEl = document.getElementById( 'img-container' );
+
+		var params = {
+			amount:     this.random(1, 100),
+			iterations: this.random(1, 100),
+			quality:    this.random(1, 100),
+			seed:       this.random(1, 100)
+		};
+
+		loadImage( imagePath, function ( img ) {
+			glitch( params )
+				.fromImage( img )
+				.toDataURL()
+				.then( function( dataURL ) {
+					var imageEl = new Image();
+					imgContainerEl.style.backgroundImage = "url("+ dataURL +")";
+				} );
+		} );
+
+		function loadImage ( src, callback ) {
+			var imageEl = new Image();
+			imageEl.onload = function () {
+				callback( imageEl );
+			};
+			imageEl.src = src;
+		}
+
+	}
+
+	random(min, max) {
+		return Math.floor(Math.random() * (max - min + 1)) + min;
+	}
+};
+
+const app = new App();
